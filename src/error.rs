23698c3950e9f510dd3bf47e716ae2f9ed0d30da
Aditya Kresna <aditya.kresna@outlook.co.id>
2024-05-15 18:10:18 +0700

    feat: onchain & offchain metadata

--- src/error.rs
+++ src/error.rs
@@ -4,8 +4,12 @@ pub type Result<T> = core::result::Result<T, self::Error>;
 pub enum Error {
     #[error("IO error")]
     IOError(#[from] std::io::Error),
+    #[error("Subxt error")]
+    SubxtError(#[from] subxt::Error),
     #[error("Connection to internal database is broken")]
     StoreConnectionBroken,
+    #[error("Bad metadata processing")]
+    BadMetadataProcessing,
 }
 
 impl actix_web::ResponseError for Error {
--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,39 @@
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 pub mod error;
+pub mod metadata;
 
 pub use error::{Error, Result};
 pub use nagara_logging::{debug, error, info, warn};
 
-pub const INDEX_TESTNET: &str = "testnet_files";
-pub const INDEX_MAINNET: &str = "mainnet_files";
+type ChainClient = subxt::OnlineClient<subxt::PolkadotConfig>;
+type FileOnchainMetadata =
+    crate::metadata::api::runtime_types::nagara_pda_files::pallet::FileInformation<
+        subxt::utils::AccountId32,
+        u128,
+    >;
+
+pub const ENV_MAINNET_URL: &str = "MAINNET_URL";
 pub const ENV_STORE_KEY: &str = "STORE_KEY";
 pub const ENV_STORE_URL: &str = "STORE_URL";
+pub const ENV_TESTNET_URL: &str = "TESTNET_URL";
+pub const INDEX_MAINNET: &str = "mainnet_files";
+pub const INDEX_TESTNET: &str = "testnet_files";
+
+async fn get_chain_client(mainnet: bool, store_vars: &StoreVariables) -> Result<ChainClient> {
+    let url = if mainnet {
+        store_vars.mainnet_url.clone()
+    } else {
+        store_vars.testnet_url.clone()
+    };
+    let chain_client = if url.starts_with("wss://") || url.starts_with("https://") {
+        ChainClient::from_url(url).await?
+    } else {
+        ChainClient::from_insecure_url(url).await?
+    };
+
+    Ok(chain_client)
+}
 
 #[derive(serde::Deserialize)]
 struct QueryParams {
@@ -17,34 +42,118 @@ struct QueryParams {
     mainnet: bool,
 }
 
-#[derive(serde::Serialize, serde::Deserialize)]
+#[derive(serde::Deserialize, serde::Serialize, Clone)]
 struct FileOffchainMetadata {
     id: nagara_identities::public::PublicKey,
     filename: String,
     content_type: String,
-    length: u64,
-    uploader: nagara_identities::public::PublicKey,
-    uploaded_at: u32,
+    uploaded_at: chrono::DateTime<chrono::Utc>,
     download_counter: u64,
+    descriptions: String,
+}
+
+impl FileOffchainMetadata {
+    async fn try_get_full_metadata(&self, chain_client: &ChainClient) -> Result<FileMetadata> {
+        let id = subxt::utils::AccountId32(self.id.to_bytes());
+        let storage_call = metadata::api::storage().pda_files().files(id);
+        let onchain_info = chain_client
+            .storage()
+            .at_latest()
+            .await?
+            .fetch(&storage_call)
+            .await?
+            .ok_or(Error::BadMetadataProcessing)?;
+        let file_metadata = FileMetadata::from((self.clone(), onchain_info));
+
+        Ok(file_metadata)
+    }
+}
+
+#[derive(serde::Deserialize, serde::Serialize)]
+struct FileMetadata {
+    id: nagara_identities::public::PublicKey,
+    uploader: nagara_identities::public::PublicKey,
+    big_brother: nagara_identities::public::PublicKey,
+    servicer: nagara_identities::public::PublicKey,
+    owner: nagara_identities::public::PublicKey,
+    #[serde(with = "hex::serde")]
+    transfer_fee: [u8; 16],
+    #[serde(with = "hex::serde")]
+    download_fee: [u8; 16],
+    size: u64,
     #[serde(with = "hex::serde")]
     hash: [u8; 32],
+    filename: String,
+    content_type: String,
+    uploaded_at: chrono::DateTime<chrono::Utc>,
+    download_counter: u64,
     descriptions: String,
+}
+
+impl From<(FileOffchainMetadata, FileOnchainMetadata)> for FileMetadata {
+    fn from(value: (FileOffchainMetadata, FileOnchainMetadata)) -> Self {
+        let id = value.0.id;
+        let uploader =
+            nagara_identities::public::PublicKey::try_from(value.1.uploader.0.as_ref()).unwrap();
+        let big_brother =
+            nagara_identities::public::PublicKey::try_from(value.1.big_brother.0.as_ref()).unwrap();
+        let servicer =
+            nagara_identities::public::PublicKey::try_from(value.1.servicer.0.as_ref()).unwrap();
+        let owner =
+            nagara_identities::public::PublicKey::try_from(value.1.owner.0.as_ref()).unwrap();
+        let size = value.1.size;
+        let hash = value.1.hash;
+        let filename = value.0.filename;
+        let content_type = value.0.content_type;
+        let uploaded_at = value.0.uploaded_at;
+        let download_counter = value.0.download_counter;
+        let descriptions = value.0.descriptions;
+        let transfer_fee = value.1.transfer_fee.to_le_bytes();
+        let download_fee = if let Some(download_fee) = value.1.download_fee {
+            download_fee.to_le_bytes()
+        } else {
+            [0; 16]
+        };
+
+        Self {
+            id,
+            uploader,
+            big_brother,
+            servicer,
+            owner,
+            transfer_fee,
+            download_fee,
+            size,
+            hash,
+            filename,
+            content_type,
+            uploaded_at,
+            download_counter,
+            descriptions,
+        }
+    }
 }
 
 #[derive(Clone)]
 struct StoreVariables {
+    mainnet_url: String,
     store_key: String,
     store_url: String,
+    testnet_url: String,
 }
 
 impl Default for StoreVariables {
     fn default() -> Self {
+        let mainnet_url = std::env::var(ENV_MAINNET_URL).unwrap();
         let store_key = std::env::var(ENV_STORE_KEY).unwrap();
         let store_url = std::env::var(ENV_STORE_URL).unwrap();
+        let testnet_url = std::env::var(ENV_TESTNET_URL).unwrap();
 
         Self {
+            mainnet_url,
             store_key,
             store_url,
+            testnet_url,
         }
     }
 }
@@ -57,7 +166,7 @@ async fn reject_unmapped_handler() -> impl actix_web::Responder {
 async fn get_file_info(
     store_vars: actix_web::web::Data<StoreVariables>,
     query_param: actix_web::web::Query<QueryParams>,
-) -> actix_web::Result<actix_web::web::Json<Vec<FileOffchainMetadata>>> {
+) -> actix_web::Result<actix_web::web::Json<Vec<FileMetadata>>> {
     let client = meilisearch_sdk::client::Client::new(
         store_vars.store_url.clone(),
         Some(store_vars.store_key.clone()),
@@ -68,20 +177,30 @@ async fn get_file_info(
     } else {
         INDEX_TESTNET
     };
-    let results = client
+    let search_results = client
         .index(index)
         .search()
         .with_query(&query_param.search)
         .execute::<FileOffchainMetadata>()
         .await
         .map_err(|_| Error::StoreConnectionBroken)?;
-    let results = results
+    let offchain_results = search_results
         .hits
         .into_iter()
         .map(|x| x.result)
         .collect::<Vec<FileOffchainMetadata>>();
+    let mut full_results = Vec::with_capacity(offchain_results.len());
 
-    Ok(actix_web::web::Json(results))
+    if !offchain_results.is_empty() {
+        let chain_client = get_chain_client(query_param.mainnet, &store_vars).await?;
+
+        for x in offchain_results {
+            let full_metadata = x.try_get_full_metadata(&chain_client).await?;
+            full_results.push(full_metadata);
+        }
+    }
+
+    Ok(actix_web::web::Json(full_results))
 }
 
 #[tokio::main]
